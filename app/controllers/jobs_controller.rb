71a77adee90d236240f88cfbfcbf9c7d9b172023
sunmeg <atpope5@gmail.com>
2014-09-18 12:31:35 -0400

    user can delete job posting

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -36,6 +36,12 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
   def job_params
     params.require(:job).permit(:title, :description, :company_id)
