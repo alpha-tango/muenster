588dfd8527fc4a2a070ef287adfbf96ef400578f
sunmeg <atpope5@gmail.com>
2014-09-18 11:20:25 -0400

    user can add new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,4 +7,22 @@ class CompaniesController < ApplicationController
   @company = Company.find(params[:id])
   @company_jobs = @company.jobs
   end
+
+  def new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :location, :industry, :description, :website)
+  end
 end
